bcfcd357cfda68258b073a12c9db6c4ee03538f6
Taras Ivashchuk <tvishy@disroot.org>
2025-11-09 16:17:29 +0200

    Annotation Self replaced to CleaUpFile with the __future__ annotations

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
-from typing import Any, Self
+from __future__ import annotations
+from typing import Any
 
 
 class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self) -> Self:
+    def __enter__(self) -> CleanUpFile:
         return self
 
     def __exit__(
