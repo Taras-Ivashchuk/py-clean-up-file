261e6e12ae83cd090a62a5b944e5e623d72e9ce8
Taras Ivashchuk <tvishy@disroot.org>
2025-11-09 16:12:30 +0200

    Initial commit

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
+from typing import Any, Self
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> Self:
+        return self
+
+    def __exit__(
+            self,
+            exc_type: Any,
+            exc_value: Any,
+            exc_traceback: Any
+    ) -> None:
+        import os
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
